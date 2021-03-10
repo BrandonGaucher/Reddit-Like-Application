69b8c466fb663edf0692a3939f8a910fde796fbb
BrandonGaucher <brandonjgaucher@gmail.com>
2021-03-10 13:50:27 -0800

    added router and backend funcitonality

---
+++ client/src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+//page components
+import Nav from '../components/nav'
+
+const Home = () =>{
+
+    return(
+<div className="home">
+<Nav />
+</div>
+    );
+}
+
+export default Home;
