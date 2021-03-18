68a791684054d7caa4afbc78b5b579a7d52f5e11
Ben-Fitz <benfitzharris@aol.com>
2021-03-17 17:13:31 -0700

    Prelim Favourites
    Favourites layout preliminary, placeholders within table will need to add functionality for the actual pages and tailored favourites of client

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,12 +2,15 @@ import React from 'react'
 
 //page components
 import Nav from '../components/nav'
+import Favourites from '../components/favourites'
 
 const Home = () =>{
 
     return(
 <div className="home">
+
 <Nav />
+<Favourites />
 </div>
     );
 }
